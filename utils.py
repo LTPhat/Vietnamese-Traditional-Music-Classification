659a6d882de6b5b643f936a51988987e333defb1
LTPhat <102889533+LTPhat@users.noreply.github.com>
2023-07-29 10:56:01 +0700

    add ensemble prediction

--- utils.py
+++ utils.py
@@ -218,7 +218,7 @@ def predict_new30s(audio_dir, model, save_dir = TEST_IMAGES_ROOT):
 
 def predict_new(audio_dir, src_folder, model, save_dir, unit_length = 661500):
     """
-    Predict audio of any length
+    Predict audio of any length using one model
     Split each audio into several equal samples which length = unit_length (661500 = 30s), then feed to NN
     Get predict class by votting each sample's prediction
 
@@ -322,4 +322,119 @@ def predict_new(audio_dir, src_folder, model, save_dir, unit_length = 661500):
     return y_pred_index, y_pred_class
 
 
-# INSTALL FFMPEG TO USE AUDIO PYDUB***************************************
+
+def PROD_predict(audio_dir, src_folder, save_dir, model1, model2, model3, unit_length = 661500):
+    """
+    Predict audio of any length using PROD fusion of three models predicted probability vectors
+    Split each audio into several equal sample which length = unit_length, then feed to NN
+    Get predict class by most votting of each sample's prediction
+
+    Input:
+    - audio_dir: List of audio directory to predict
+    - src_folder: Dir of folder containning audio_dir
+    - model: Model to predict
+    - save_dir: Directory to save log-mel-spec image of samples splitted from each audio in audio_dir
+    Output:
+    - y_pred_index: List of index predicted of each audio in audio_dir
+    - y_pred_class: Respective class predicted of y_pred_index
+    """
+
+    def mp3_2_wav(dir, dst, sample_rate = 22050):
+        """
+        Convert mp3 to wav and save wav file to dst
+        Input: dir (mp3)
+        """
+        # convert wav to mp3.
+        sound = AudioSegment.from_mp3(dir)
+        sound.set_frame_rate(sample_rate)
+        sound.export(dst, format="wav")
+
+
+    def process(samples_split, save_dir, file_name, is_saved):
+        """
+        End to end processing steps of each audio
+
+        Input:
+        - samples_split: List of samples splitted from each audio in audio_dir
+        - save_dir: Directory to save log-mel-spec image of samples splitted from each audio in audio_dir
+        - is_save: If False, do not save log-mel-spec image of samples, just make prediction
+
+        Output:
+        - np.array(samples_db_list): A batch of samples of each audio file (nums_of_sample, input_shape[0], input_shape[1], 3) to feed to NN
+        """
+        samples_db_list = []
+        for i, sample in enumerate(samples_split):
+            S = lb.feature.melspectrogram(y = sample, sr=sr)
+            S_db = lb.amplitude_to_db(S, ref=np.max)
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+            sample_root = save_dir + "\\{}".format(file_name) + "_sample{}".format(i) + ".png"
+            plt.imsave(sample_root, S_db)
+            image = load_img(sample_root, target_size=(INPUT_SHAPE[0], INPUT_SHAPE[1], 3))
+            img_array = img_to_array(image)
+            img_array = img_array / 255
+            samples_db_list.append(img_array)
+            if not is_saved: # Not save mode
+                for file in os.listdir(save_dir):
+                    if file.endswith('.png'):
+                        os.remove(save_dir + "\\" + file)
+        return np.array(samples_db_list)
+
+    # Define result
+    y_pred_index = []
+    y_pred_class = []
+
+    # List of samples of each audio
+    samples_split = []
+    y_pred_split = []
+
+    for dir in audio_dir:
+        if dir.endswith(".mp3"):
+            # Get file name
+            wav_dir = src_folder + "\\" + dir.split("\\")[-1][:-4] + ".wav"
+            mp3_2_wav(dir, wav_dir)
+            dir = wav_dir       # Take wav dir for sampling
+        print(dir)
+        audio, sr = lb.load(dir)
+        if (len(audio) >= unit_length):
+            # Number of sample of each audio
+            nums_of_samples = len(audio) // unit_length
+        else:
+            err = "Audio length must be greater than 30s"
+            print(err)
+            return err
+        for i in range(0, nums_of_samples):
+            curr_sample = audio[i * unit_length : i * unit_length + unit_length]
+            if (len(curr_sample) != unit_length): # Cannot sampling this curr_sample
+                break
+            samples_split.append(audio[i * unit_length : i * unit_length + unit_length])
+
+        file_name = dir.split("\\")[-1][:-4]
+
+        input_data = process(samples_split, save_dir, file_name, False)
+
+        pred_candidates1 = model1.predict(input_data, verbose = 0)
+
+        pred_candidates2 = model2.predict(input_data, verbose = 0)
+
+        pred_candidates3 = model3.predict(input_data, verbose = 0)
+
+        PROD_probs = []
+
+        # PROD fusion
+        for i in range(pred_candidates1.shape[0]):
+          PROD_probs.append(1/3 * pred_candidates1[i] * pred_candidates2[i] * pred_candidates3[i])
+
+        pred_index_candidates = [np.argmax(sample) for sample in PROD_probs]
+
+        pred_index = max(pred_index_candidates, key = pred_index_candidates.count)
+        pred_class = class_list[pred_index]
+
+        y_pred_index.append(pred_index)
+        y_pred_class.append(pred_class)
+
+        # Reset samples_split after passing one dir of audio_dir
+        samples_split = []
+
+    return y_pred_index, y_pred_class
+
